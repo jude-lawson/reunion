cd9d76b65f855d1325e4fcef1623575d2ecb03fe
Jude Lawson <lawsonjdutton@gmail.com>
2018-04-05 15:27:28 -0600

    Initial commit. Adds tests for initial Activity class.

--- test/activity_test.rb
+++ test/activity_test.rb
@@ -4,4 +4,17 @@ require 'minitest/pride'
 require './lib/activity'
 
 class ActivityTest < Minitest::Test
+
+  def setup
+    @activity = Activity.new("hiking")
+  end
+
+  def test_activity_exists
+    assert_instance_of Activity, @activity
+  end
+
+  def test_activity_can_have_a_namew
+    assert_equal "hiking", @activity.name
+  end
+  
 end
