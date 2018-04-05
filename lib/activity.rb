eff06c2e94706f49ff7457f9d329f444638b7397
Jude Lawson <lawsonjdutton@gmail.com>
2018-04-05 17:03:57 -0600

    Adds split_cost method to Activity class

--- lib/activity.rb
+++ lib/activity.rb
@@ -13,6 +13,19 @@ class Activity
     participants[name] = initial_payment
   end
 
+  def split_cost
+    num_participants = @participants.length
+    return 0 if num_participants == 0
+    if @cost % num_participants == 0
+      @cost / num_participants
+    else
+      base_cost = @cost / num_participants
+      cost_with_remainder = base_cost + (@cost % num_participants)
+      [cost_with_remainder, base_cost]
+    end
+  end
+
+
 
 
 end
