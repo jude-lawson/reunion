d8619b5430619381658072287ee1e5d22b7780a8
Jude Lawson <lawsonjdutton@gmail.com>
2018-04-05 16:12:25 -0600

    Adds additional tests for Activity class

--- test/activity_test.rb
+++ test/activity_test.rb
@@ -6,7 +6,7 @@ require './lib/activity'
 class ActivityTest < Minitest::Test
 
   def setup
-    @activity = Activity.new("hiking")
+    @activity = Activity.new("hiking", 100)
   end
 
   def test_activity_exists
@@ -16,5 +16,24 @@ class ActivityTest < Minitest::Test
   def test_activity_can_have_a_namew
     assert_equal "hiking", @activity.name
   end
-  
+
+  def test_activity_has_a_cost
+    assert_equal 100, @activity.costgs
+  end
+
+  def test_participants_can_be_added_to_activity
+    @activity.add_participant("Jules", 20)
+  end
+
+  def test_participants_can_be_retrieved
+    @activity.add_participant(:Jules, 20)
+    @activity.add_participant(:Ida, 20)
+    @activity.add_participant(:Jerry, 0)
+    
+    assert_instance_of Hash, @activity.participants
+    assert_equal 20, @activity.participants[:Jules]
+    assert_equal 20, @activity.participants[:Ida]
+    assert_equal 10, @activity.participants[:Jerry]
+  end
+
 end
