988ea4e4c3fdedaa70470f5956f2e4288da5bed3
Jude Lawson <lawsonjdutton@gmail.com>
2018-04-05 21:35:55 -0600

    Adds in method to evaluate total payments per participant for Reunion

--- lib/reunion.rb
+++ lib/reunion.rb
@@ -25,4 +25,26 @@ class Reunion
     end
     costs.sum
   end
+
+  def evaluate_payment
+    all_names = @activities.map do |activity|
+      activity.participants.keys
+    end.flatten.uniq
+    # Evaluate amount owed for each activity
+    amounts_per_activity = @activities.map do |activity|
+      activity.evaluate_amount_owed(activity.participants)
+    end
+
+    payments_owed = {}
+    all_names.each do |name|
+      amounts_per_activity.each do |activity_amount|
+        if payments_owed[name]
+          payments_owed[name] += activity_amount[name]
+        else
+          payments_owed[name] = activity_amount[name]
+        end
+      end
+    end
+    payments_owed
+  end
 end
